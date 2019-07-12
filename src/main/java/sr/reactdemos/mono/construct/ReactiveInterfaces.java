a693ab9b304a5c46195b1ee779c0cb8bd30e4e0f
sravale <sravale@vmware.com>
2019-07-12 12:28:24 +0530

    [Initial]

---
+++ src/main/java/sr/reactdemos/mono/construct/ReactiveInterfaces.java
@@ -0,0 +1,98 @@
+package sr.reactdemos.mono.construct;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import reactor.core.publisher.Flux;
+
+public class ReactiveInterfaces {
+
+  public static void main(final String[] args) {
+
+    final Publisher<Integer> customPublisher = subscriber -> {
+      subscriber.onSubscribe(new CustomSubscription(subscriber));
+    };
+
+    final AtomicReference<Subscription> subscriptionReference = new AtomicReference<>();
+
+    final Subscriber<Integer> customSubscriber = new Subscriber<Integer>() {
+
+      @Override
+      public void onSubscribe(final Subscription s) {
+        System.out.println("Subscribed, requesting 1");
+        s.request(1);
+        subscriptionReference.set(s);
+        // s.request(2);
+        // s.request(1);
+        // s.request(20); // This won't work as only 10 items can be published
+      }
+
+      @Override
+      public void onNext(final Integer t) {
+        System.out.println("Published: " + t);
+        // System.out.println("Cancelling");
+        // subscriptionReference.get().cancel();
+        // This won't work as subscription is cancelled
+
+        if (t < 9) {
+          System.out.println("requesting: " + (t + 1));
+          subscriptionReference.get().request(1);
+          System.out.println("requested: " + (t + 1));
+        }
+        System.out.println("on next complete: " + t);
+      }
+
+      @Override
+      public void onError(final Throwable t) {
+        System.out.println("Error occured: " + t.getMessage());
+
+      }
+
+      @Override
+      public void onComplete() {
+        System.out.println("Completed");
+
+      }
+    };
+
+    // customPublisher.subscribe(customSubscriber);
+
+    Flux.range(0, 10)//
+        .subscribe(customSubscriber);
+
+  }
+
+  public static class CustomSubscription implements Subscription {
+
+    private final Subscriber<? super Integer> subscriber;
+
+    private int publishedCount = 0;
+
+    public CustomSubscription(final Subscriber<? super Integer> subscriber) {
+      this.subscriber = subscriber;
+    }
+
+    @Override
+    public void request(final long n) {
+
+      if (this.publishedCount >= 10) {
+        return;
+      }
+      for (final int i = 0; i < n && this.publishedCount < 10; this.publishedCount++) {
+        this.subscriber.onNext(this.publishedCount);
+      }
+      if (this.publishedCount >= 10) {
+        this.subscriber.onComplete();
+      }
+
+    }
+
+    @Override
+    public void cancel() {
+
+    }
+  }
+}
---
+++ src/main/java/sr/reactdemos/mono/construct/SimpleMonoConstructions.java
@@ -0,0 +1,36 @@
+package sr.reactdemos.mono.construct;
+
+import reactor.core.publisher.Mono;
+
+public class SimpleMonoConstructions {
+
+  public static void main(final String[] args) {
+
+    // Simple subscription
+    Mono.just("Hello world!")//
+        .subscribe(System.out::println);
+
+    final Mono<String> mono = Mono.just("Multiple subscriptions");
+
+    // All below are different subscribers to the same mono
+    System.out.println(mono.block());
+    System.out.println(mono.block());
+    mono.subscribe(System.out::println);
+    mono.subscribe(System.out::println);
+    mono.subscribe(System.out::println);
+
+    // returns T so that can be assigned to any type of Mono
+    final Mono<String> emptyMono = Mono.empty();
+    System.out.println("Empty mono value: " + emptyMono.block());
+
+    // mono with error
+    final Mono<Object> errorMono = Mono.error(() -> new RuntimeException("Error!"));
+    try {
+    errorMono.block();
+    } catch (final RuntimeException e) {
+      System.out.println(e.getMessage());
+    }
+
+  }
+
+}
